7f05557e0a632131db988280f2b6e80466261987
NguyenPhuThinh2603 <nguyenphuthinh26@gmail.com>
2024-12-04 16:13:20 +0700

    Updates

---
+++ nguyenphuthinh_2122110426/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nguyenphuthinh_2122110426.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Allcategory()
+        {
+            return View();
+        }
+        public ActionResult ProductCategory()
+        {
+            return View();
+        }
+    }
+}
---
+++ nguyenphuthinh_2122110426/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="nguyenphuthinh_2122110426.MvcApplication" Language="C#" %>
